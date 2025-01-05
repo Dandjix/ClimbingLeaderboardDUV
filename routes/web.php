87863e3e95d28983526b96eff45f458ff2ff5dba
Timon Dubreuil <timondubreuil@outlook.com>
2025-01-05 23:05:09 +0100

    réparé login logout

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 use App\Http\Controllers\Auth\LoginController;
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
+
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 
